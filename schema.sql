876049d56b125c5ad79512c08d980690f1ac1582
esanche1 <edcsanchez@yahoo.com>
2016-10-04 11:36:48 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,3 +4,15 @@ create table entries (
   title text not null,
   'text' text not null
 );
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print 'Initialized the database.'
